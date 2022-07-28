145c8a19cb61cd63619d4493a56f2e4aa5f6581e
zhangtao25 <tzhangm@trip.com>
2022-07-28 11:03:43 +0800

    upd

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MockController } from './apps/mock/mock.controller';
 import { RequestModule } from './apps/request/request.module';
+import {join} from "path";
+import { ServeStaticModule } from '@nestjs/serve-static'
 
 @Module({
-  imports: [RequestModule],
+  imports: [RequestModule,    ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'client'),
+  }),],
   controllers: [AppController, MockController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ async function bootstrap() {
   const { AppModule } = await import('./app.module');
 
   const app = await NestFactory.create(AppModule);
+  app.use(function (req, res, next) {
+    const old_url = req.url
+    if (old_url.includes('/api')) {
+      req.url = old_url.replace('/api', '')
+    }
+    next()
+  })
   app.enableCors();
   await app.listen(8080);
 }
